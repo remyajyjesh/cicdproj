75a69bef2e4d876fb350f5d5ebefd3c26c32fc68
remurali <you@example.com>
2023-01-04 15:17:58 +0530

    first commit

---
+++ cicdproj/src/main/java/com/example/cicdproj/CicdprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdprojApplication.class, args);
+	}
+
+}
---
+++ cicdproj/src/test/java/com/example/cicdproj/CicdprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
